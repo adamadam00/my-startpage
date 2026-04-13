563c41cfc6b7a89e01486a085921de072c894067
adamadam00 <51180362+adamadam00@users.noreply.github.com>
2026-04-13 20:09:53 +1000

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ function ClockWidget() {
   const [now, setNow] = useState(new Date())
   useEffect(() => {
     const id = setInterval(() => setNow(new Date()), 1000)
-    return (
-    <>) => clearInterval(id)
+    return () => clearInterval(id)
   }, [])
   const hm = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
   const date = now.toLocaleDateString([], { weekday: 'short', month: 'short', day: 'numeric' })
