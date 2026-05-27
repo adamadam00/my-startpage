d8177efe069ce0e8c1c1d71338682b073c2caa46
adamadam00 <51180362+adamadam00@users.noreply.github.com>
2026-05-27 21:46:28 +1000

    sss

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -681,7 +681,7 @@ export default function Sections({
   const sensors = useSensors(
     useSensor(PointerSensor, {
       activationConstraint: { 
-        distance: 8,
+        distance: 4,
       },
     })
   );
